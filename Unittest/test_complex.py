cb9f737da8231e7361a7c701d2b75ca5f3e7d2d3
ahojjasomalex <xbazoa00@vutbr.cz>
2023-04-20 17:24:29 +0200

    refactor

--- Unittest/test_complex.py
+++ Unittest/test_complex.py
@@ -14,47 +14,38 @@ class ParserComplexTestCase(unittest.TestCase):
         del self.p
 
     def test_parser_add_sub_mult_div_1(self):
-        
         data = '5+2-6*3/6'
         self.assertEqual(self.p.parser.parse(data), 4)
 
     def test_parser_add_sub_mult_div_2(self):
-        
         data = '100*100/100+4/2'
         self.assertEqual(self.p.parser.parse(data), 102)
 
     def test_parser_add_sub_mult_div_3(self):
-        
         data = '42/3*8-7/10'
         self.assertEqual(self.p.parser.parse(data), 111.3)
 
     def test_parser_zero_div(self):
-        
         data = '2/(2-2)'
         self.assertEqual(self.p.parser.parse(data), None)
 
     def test_op_precedence_1(self):
-        
         data = '3+3*3'
         self.assertEqual(self.p.parser.parse(data), 12)
 
     def test_op_precedence_2(self):
-        
         data = '3+3/3'
         self.assertEqual(self.p.parser.parse(data), 4)
 
     def test_op_precedence_3(self):
-        
         data = '2^4*2`16'
         self.assertEqual(self.p.parser.parse(data), 1.0905077326652577)
 
     def test_op_precedence_4(self):
-        
         data = '2^4*(2`16)'
         self.assertEqual(self.p.parser.parse(data), 64)
 
     def test_frenzy_1(self):
-        
         data = '3+3*3+(6-1)!/8+2^4/(2`16)+4!-3*5+2'
         self.assertEqual(self.p.parser.parse(data), 42)
 
