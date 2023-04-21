096ce4ee1c22e10a1c2d00a4ed28d2e5f3c0d4da
ahojjasomalex <xbazoa00@vutbr.cz>
2023-04-21 21:57:47 +0200

    added gui prototype

--- main.py
+++ main.py
@@ -3,22 +3,6 @@ import sys
 import ply.lex
 
 from CalcUtils import parser
-import PySimpleGUI as sg
-
-layout = [[sg.Txt('' * 10)],
-          [sg.Text('', size=(15, 1), font=('Helvetica', 18),
-                   text_color='black', key='input')],
-          [sg.Txt('' * 10)],
-          [sg.ReadFormButton('c'), sg.ReadFormButton('«'), sg.ReadFormButton('^'), sg.ReadFormButton('`')],
-          [sg.ReadFormButton('7'), sg.ReadFormButton('8'), sg.ReadFormButton('9'), sg.ReadFormButton('/')],
-          [sg.ReadFormButton('4'), sg.ReadFormButton('5'), sg.ReadFormButton('6'), sg.ReadFormButton('*')],
-          [sg.ReadFormButton('1'), sg.ReadFormButton('2'), sg.ReadFormButton('3'), sg.ReadFormButton('-')],
-          [sg.ReadFormButton('.'), sg.ReadFormButton('0'), sg.ReadFormButton('='), sg.ReadFormButton('+')],
-          ]
-
-form = sg.FlexForm('CALCULATOR', default_button_element_size=(5, 2),
-                   auto_size_buttons=False, grab_anywhere=False)
-form.Layout(layout)
 
 
 def main():
